3117def47d578cb47e59320b3ee78d86c2383dbb
Edson-Padilha <b12206@bra01ad01.local>
2022-01-28 17:34:12 -0300

    Incluindo registro de transação, contador de colunas total

--- retorno.py
+++ retorno.py
@@ -11,11 +11,21 @@ linhas = arq.readlines()
 
 for col in linhas:
     cont = cont + 1  
-   
+    ir = str(col[0])#Identificador de registro
+    tie = str((col[2],col[3]))#Tipo de inscrição da empresa
+
+    
+    if ir == "1":
+        rt =  "Registro de Transação"
+
     ocorrencia = str(col[108]+col[109])
 
-    if cont != 1:
+    if cont != 1:        
+        print(f"{rt}")
         print(f"Linha : {cont} - Ocorrência: {ocorrencia}")
         print("-"*50)
 
-print(f"Total de documentos: {cont - 1}")
+print(f"Total de colunas: {len(col)}")#Conta a quantidade de colunas
+if len(col) > 400:
+    print(f"Quantidade de colunas excedente no arquivo. Linha: {cont -1}")
+print(f"Total de documentos: {cont - 1}")#Total de registros no retorno
